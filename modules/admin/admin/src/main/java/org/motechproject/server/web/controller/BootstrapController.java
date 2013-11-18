4fb8a506170c272cb356dd25100cb562e61c3de5
Subhrajit Roy <sroy@thoughtworks.com>
2013-11-18 15:56:33 +0530

    Added Bootstrap,Login and Startup Controllers
    Change-Id: Ic6acd3cb597c6f82f93ceff934f950761b0f2329

---
+++ modules/admin/admin/src/main/java/org/motechproject/server/web/controller/BootstrapController.java
@@ -0,0 +1,144 @@
+package org.motechproject.server.web.controller;
+
+import org.ektorp.CouchDbInstance;
+import org.ektorp.DbAccessException;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.motechproject.config.domain.BootstrapConfig;
+import org.motechproject.config.domain.ConfigSource;
+import org.motechproject.config.domain.DBConfig;
+import org.motechproject.config.service.ConfigurationService;
+import org.motechproject.server.startup.StartupManager;
+import org.motechproject.server.web.form.BootstrapConfigForm;
+import org.motechproject.server.web.validator.BootstrapConfigFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class BootstrapController {
+    public static final String BOOTSTRAP_CONFIG_VIEW = "bootstrapconfig";
+    private static final String DB_URL_SUGGESTION = "http://localhost:5984/";
+    private static final String TENANT_ID_DEFAULT = "DEFAULT";
+    private static final String ERRORS = "errors";
+    private static final String WARNINGS = "warnings";
+    private static final String SUCCESS = "success";
+    private static final int CONNECTION_TIMEOUT = 4000; //ms
+
+    @Autowired
+    private StartupManager startupManager;
+
+    @Autowired
+    private ConfigurationService configurationService;
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(new BootstrapConfigFormValidator());
+    }
+
+    @RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
+    public ModelAndView bootstrapForm() {
+        if (!startupManager.isBootstrapConfigRequired()) {
+            return new ModelAndView(REDIRECT_HOME);
+        }
+
+        ModelAndView bootstrapView = new ModelAndView(BOOTSTRAP_CONFIG_VIEW);
+        bootstrapView.addObject("bootstrapConfig", new BootstrapConfigForm());
+        bootstrapView.addObject("username", System.getProperty("user.name"));
+        bootstrapView.addObject("dbUrlSuggestion", DB_URL_SUGGESTION);
+        bootstrapView.addObject("tenantIdDefault", TENANT_ID_DEFAULT);
+        return bootstrapView;
+    }
+
+    @RequestMapping(value = "/bootstrap", method = RequestMethod.POST)
+    public ModelAndView submitForm(@ModelAttribute("bootstrapConfig") @Valid BootstrapConfigForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            ModelAndView bootstrapView = new ModelAndView(BOOTSTRAP_CONFIG_VIEW);
+            bootstrapView.addObject("errors", getErrors(result));
+            bootstrapView.addObject("username", System.getProperty("user.name"));
+            bootstrapView.addObject("dbUrlSuggestion", DB_URL_SUGGESTION);
+            bootstrapView.addObject("tenantIdDefault", TENANT_ID_DEFAULT);
+            return bootstrapView;
+        }
+
+        BootstrapConfig bootstrapConfig = new BootstrapConfig(new DBConfig(form.getDbUrl(), form.getDbUsername(), form.getDbPassword()), form.getTenantId(), ConfigSource.valueOf(form.getConfigSource()));
+        try {
+            configurationService.save(bootstrapConfig);
+        } catch (Exception e) {
+            ModelAndView bootstrapView = new ModelAndView(BOOTSTRAP_CONFIG_VIEW);
+            bootstrapView.addObject("errors", Arrays.asList("server.error.bootstrap.save"));
+            bootstrapView.addObject("username", System.getProperty("user.name"));
+            bootstrapView.addObject("dbUrlSuggestion", DB_URL_SUGGESTION);
+            bootstrapView.addObject("tenantIdDefault", TENANT_ID_DEFAULT);
+            return bootstrapView;
+        }
+
+        startupManager.startup();
+
+        return new ModelAndView(REDIRECT_HOME);
+    }
+
+    @RequestMapping(value = "/bootstrap/verify", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, ? extends Object> verifyConnection(@ModelAttribute("bootstrapConfig") @Valid BootstrapConfigForm form, BindingResult result) {
+        Map<String, Object> response = new HashMap<>();
+
+        if (result.hasErrors()) {
+            response.put(WARNINGS, Arrays.asList("server.bootstrap.verify.error"));
+            response.put(ERRORS, getErrors(result));
+        } else {
+            try {
+                HttpClient httpClient = new StdHttpClient.Builder()
+                        .url(form.getDbUrl())
+                        .username(form.getDbUsername())
+                        .password(form.getDbPassword())
+                        .caching(false)
+                        .connectionTimeout(CONNECTION_TIMEOUT)
+                        .build();
+
+                CouchDbInstance couchDbInstance = new StdCouchDbInstance(httpClient);
+                couchDbInstance.getAllDatabases();
+
+                response.put(SUCCESS, true);
+
+            } catch (MalformedURLException e) {
+                response.put(ERRORS, Arrays.asList("server.error.invalid.dbUrl"));
+                response.put(ERRORS, Arrays.asList("server.bootstrap.verify.error"));
+                response.put(SUCCESS, false);
+            } catch (DbAccessException e) {
+                response.put(WARNINGS, Arrays.asList("server.bootstrap.verify.warning"));
+                response.put(SUCCESS, false);
+            }
+        }
+
+        return response;
+    }
+
+    private List<String> getErrors(final BindingResult result) {
+        List<ObjectError> allErrors = result.getAllErrors();
+        List<String> errors = new ArrayList<>(allErrors.size());
+
+        for (ObjectError error : allErrors) {
+            errors.add(error.getCode());
+        }
+
+        return errors;
+    }
+}
---
+++ modules/admin/admin/src/main/java/org/motechproject/server/web/controller/StartupController.java
@@ -0,0 +1,160 @@
+package org.motechproject.server.web.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.motechproject.config.domain.ConfigSource;
+import org.motechproject.config.service.ConfigurationService;
+import org.motechproject.security.service.MotechUserService;
+import org.motechproject.server.config.domain.LoginMode;
+import org.motechproject.server.config.domain.MotechSettings;
+import org.motechproject.server.startup.StartupManager;
+import org.motechproject.server.ui.LocaleService;
+import org.motechproject.server.web.form.StartupForm;
+import org.motechproject.server.web.form.StartupSuggestionsForm;
+import org.motechproject.server.web.helper.SuggestionHelper;
+import org.motechproject.server.web.validator.StartupFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+@Controller
+public class StartupController {
+
+    public static final String BUNDLE_ADMIN_ROLE = "Bundle Admin";
+    public static final String USER_ADMIN_ROLE = "User Admin";
+    public static final String EMAIL_ADMIN_ROLE = "Email Admin";
+    public static final String SECURITY_ADMIN_ROLE = "Security Admin";
+
+    @Autowired
+    private StartupManager startupManager;
+
+    @Autowired
+    private ConfigurationService configurationService;
+
+    @Autowired
+    private LocaleService localeService;
+
+    @Autowired
+    private MotechUserService userService;
+
+    @Autowired
+    private SuggestionHelper suggestionHelper;
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(new StartupFormValidator(userService, configurationService));
+    }
+
+    @RequestMapping(value = "/startup", method = RequestMethod.GET)
+    public ModelAndView startup(final HttpServletRequest request) {
+        ModelAndView view = new ModelAndView("startup");
+
+        if (startupManager.canLaunchBundles()) {
+            view.setViewName(REDIRECT_HOME);
+        } else {
+            Locale userLocale = localeService.getUserLocale(request);
+
+            ConfigSource configSource = (configurationService.loadBootstrapConfig() != null) ?
+                    configurationService.loadBootstrapConfig().getConfigSource() : ConfigSource.UI;
+
+            StartupForm startupSettings = new StartupForm();
+            startupSettings.setLanguage(userLocale.getLanguage());
+
+            view.addObject("suggestions", createSuggestions());
+            view.addObject("startupSettings", startupSettings);
+            view.addObject("languages", localeService.getAvailableLanguages());
+            view.addObject("pageLang", userLocale);
+            view.addObject("isFileMode", ConfigSource.FILE.equals(configSource));
+        }
+
+        return view;
+    }
+
+    @RequestMapping(value = "/startup", method = RequestMethod.POST)
+    public ModelAndView submitForm(@ModelAttribute("startupSettings") @Valid StartupForm form,
+                                   BindingResult result) {
+        ModelAndView view = new ModelAndView(REDIRECT_HOME);
+        ConfigSource configSource = (configurationService.loadBootstrapConfig() != null) ?
+                configurationService.loadBootstrapConfig().getConfigSource() : ConfigSource.UI;
+
+        if (result.hasErrors()) {
+
+            view.addObject("suggestions", createSuggestions());
+            view.addObject("languages", localeService.getAvailableLanguages());
+            view.addObject("loginMode", form.getLoginMode());
+            view.addObject("errors", getErrors(result));
+            view.addObject("isFileMode", ConfigSource.FILE.equals(configSource));
+
+            view.setViewName("startup");
+        } else {
+            if (ConfigSource.UI.equals(configSource)) {
+                MotechSettings settings = startupManager.getDefaultSettings();
+
+                settings.setLanguage(form.getLanguage());
+                settings.setLoginModeValue(form.getLoginMode());
+                settings.savePlatformSetting(AMQ_BROKER_URL, form.getQueueUrl());
+                settings.setProviderName(form.getProviderName());
+                settings.setProviderUrl(form.getProviderUrl());
+
+                configurationService.savePlatformSettings(settings);
+
+                if (LoginMode.REPOSITORY.equals(LoginMode.valueOf(form.getLoginMode()))) {
+                    registerAdminUser(form);
+                }
+            } else {
+                registerAdminUser(form);
+            }
+
+            startupManager.startup();
+        }
+
+        return view;
+    }
+
+    private List<String> getErrors(final BindingResult result) {
+        List<ObjectError> allErrors = result.getAllErrors();
+        List<String> errors = new ArrayList<>(allErrors.size());
+
+        for (ObjectError error : allErrors) {
+            errors.add(error.getCode());
+        }
+
+        return errors;
+    }
+
+    private StartupSuggestionsForm createSuggestions() {
+        StartupSuggestionsForm suggestions = new StartupSuggestionsForm();
+
+        String queueUrl = suggestionHelper.suggestActivemqUrl();
+
+        if (StringUtils.isNotBlank(queueUrl)) {
+            suggestions.addQueueSuggestion(queueUrl);
+        }
+
+        return suggestions;
+    }
+
+    private void registerAdminUser(StartupForm form) {
+        String login = form.getAdminLogin();
+        String password = form.getAdminPassword();
+        String email = form.getAdminEmail();
+        Locale locale = new Locale(form.getLanguage());
+
+        List<String> roles = Arrays.asList(USER_ADMIN_ROLE, BUNDLE_ADMIN_ROLE, EMAIL_ADMIN_ROLE, SECURITY_ADMIN_ROLE);
+
+        userService.register(login, password, email, null, roles, locale);
+    }
+}
